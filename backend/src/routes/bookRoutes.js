9392ff8f7850a3caf5bc72dcf29b94a8d2665595
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-22 12:49:07 +0700

    add: bookRoutes

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -10,5 +10,28 @@ const routes = [
   {
     method: 'POST',
     path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookid}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookid}',
+    handler: updateBookHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookid}',
+    handler: deleteBookHandler,
   },
 ];
+
+module.exports = routes;
