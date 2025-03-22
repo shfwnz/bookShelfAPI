3f8e27f591c927c663d15931151489ec9fdf178b
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-22 15:36:10 +0700

    add: sidequest

--- backend/src/handlers/booksHandler.js
+++ backend/src/handlers/booksHandler.js
@@ -63,12 +63,42 @@ const addBookHandler = (request, h) => {
     .code(201);
 };
 
-const getAllBooksHandler = () => ({
-  status: 'success',
-  data: {
-    books: books.map(({ id, name, publisher }) => ({ id, name, publisher })),
-  },
-});
+const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+
+  let filteredBooks = books;
+
+  if (name) {
+    filteredBooks = filteredBooks.filter((book) =>
+      book.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+
+  if (reading !== undefined) {
+    const isReading = reading === '1';
+    filteredBooks = filteredBooks.filter((book) => book.reading === isReading);
+  }
+
+  if (finished !== undefined) {
+    const isFinished = finished === '1';
+    filteredBooks = filteredBooks.filter(
+      (book) => book.finished === isFinished
+    );
+  }
+
+  return h
+    .response({
+      status: 'success',
+      data: {
+        books: filteredBooks.map(({ id, name, publisher }) => ({
+          id,
+          name,
+          publisher,
+        })),
+      },
+    })
+    .code(200);
+};
 
 const getBookByIdHandler = (request, h) => {
   const { bookId } = request.params;
